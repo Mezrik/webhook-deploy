2fea1345d06e4c5a437ceb6ef8c17d84354144c2
Martin Petr <martin.petr.276@gmail.com>
2020-10-13 22:56:48 +0200

    Startup bash script, added pm2 process manager

---
+++ start.sh
@@ -0,0 +1,12 @@
+pre#!/bin/bash
+LOG_SEPARATOR="------------------------------------------"
+STARTUP_LOG_LOCATION=./logs/startup.log
+
+if [ $(ps -e -o uid | grep $UID | grep node | grep -v grep | wc -l | tr -s "\n") -eq 0 ]
+then
+        mkdir -p logs
+        echo $LOG_SEPARATOR >> ${STARTUP_LOG_LOCATION}
+        date +"%m-%d-%Y-%T" >> ${STARTUP_LOG_LOCATION}
+        npm run start >> ${STARTUP_LOG_LOCATION} 2>&1
+        echo $LOG_SEPARATOR >> ${STARTUP_LOG_LOCATION}
+fi
