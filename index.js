53cb656b437e668c5a30be7978e5b808d328972a
Martin Petr <martin.petr.276@gmail.com>
2020-10-13 23:16:15 +0200

    Handle github webhook request

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const server = http.createServer();
 
 server.listen(config.PORT, (error) => {
   if (error) {
-    return console.error(error);
+    throw Error(`Failed to run on port ${config.PORT}`);
   }
 
   console.log(`Server is listening on port ${config.PORT}`);
@@ -26,6 +26,19 @@ server.on("request", (request, response) => {
         .digest("hex");
 
     if (request.headers["x-hub-signature"] == signature) {
+      console.log(`
+        Event "${request.headers["x-github-event"]}" in "${request.chunk.repository["full_name"]}" repository:
+          Perfrom build of "${config.REPOSITORY_LOCATION}" application and deploy it.
+      `);
+      exec("bash build.sh", (error, stdout, stderr) => {
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+        if (error !== null) {
+          console.log(`exec error: ${error}`);
+        } else {
+          console.log(`Build completed successfully.`);
+        }
+      });
     }
   });
 
